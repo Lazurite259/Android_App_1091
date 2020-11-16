3a8e8629651df33242266b98178696707da6f30a
Vinnie <Lazurite259@users.noreply.github.com>
2020-11-16 21:45:55 +0800

    P1113 finish, Open new project P7_1113

--- P1113/app/src/main/java/ncku/geomatics/p1113/MainActivity.java
+++ P1113/app/src/main/java/ncku/geomatics/p1113/MainActivity.java
@@ -1,14 +1,82 @@
 package ncku.geomatics.p1113;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements AdapterView.OnItemClickListener,
+        AdapterView.OnItemLongClickListener {
+
+    String[] memo = {"1. xxx", "2. xxx", "3. ", "4. ", "5. ", "6. "};
+    ArrayAdapter<String> adp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adp = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, memo);
+        ListView lv = findViewById(R.id.listView);
+        lv.setAdapter(adp);
+        lv.setOnItemClickListener(this);
+        lv.setOnItemLongClickListener(this);
+    }
+
+    public void onClick(View v) {
+        //開啟第二畫面
+        Intent it = new Intent();
+        it.setClass(this, MainActivity2.class);
+        startActivity(it);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent it = new Intent();
+        it.setClass(this, MainActivity2.class);
+        it.putExtra("id", position + 1);
+        String s = "";
+        if (memo[position].length() > 3) {
+            s = memo[position].substring(3);
+        }
+        it.putExtra("content", s);
+//        startActivity(it);
+        startActivityForResult(it, 999);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        memo[position] = (position + 1) + ". ";
+//        adp = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, memo);
+//        ListView lv = findViewById(R.id.listView);
+//        lv.setAdapter(adp);
+        adp.notifyDataSetChanged();
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 999 && resultCode == RESULT_OK) {
+            int gID = data.getIntExtra("id", 0);
+            String gStr = data.getStringExtra("content");
+            memo[gID - 1] = gID + ". " + gStr;
+            adp.notifyDataSetChanged();
+        }
+    }
+
+    public void call(View v){
+        Intent it3=new Intent();
+        it3.setAction(Intent.ACTION_VIEW);
+//        it3.setData(Uri.parse("tel:800"));
+        it3.setData(Uri.parse("geo:25.047095, 121.517308"));
+        startActivity(it3);
     }
 }
--- P1113/app/src/main/java/ncku/geomatics/p1113/MainActivity2.java
+++ P1113/app/src/main/java/ncku/geomatics/p1113/MainActivity2.java
@@ -2,13 +2,45 @@ package ncku.geomatics.p1113;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.opengl.ETC1;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import javax.xml.transform.Result;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent it = getIntent();
+        int gID = it.getIntExtra("id", 0);
+        String gStr = it.getStringExtra("content");
+        tvID = findViewById(R.id.textViewID);
+        tvID.setText(gID + "");
+        etC = findViewById(R.id.editTextContent);
+        etC.setText(gStr);
+    }
+
+    public void cancel(View v) {
+        finish(); //關掉此畫面
+    }
+
+    public void save(View v) {
+        Intent it2 = new Intent();
+        int vID = Integer.parseInt(tvID.getText().toString());
+        String vStr = etC.getText().toString();
+        it2.putExtra("id", vID);
+        it2.putExtra("content", vStr);
+        setResult(RESULT_OK, it2);
+        finish();
     }
 }
