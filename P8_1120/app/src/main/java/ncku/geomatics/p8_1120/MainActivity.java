b69a9aa66d23715079b2e16d0d63b12533e06c5f
Vinnie <Lazurite259@users.noreply.github.com>
2020-11-24 19:26:50 +0800

    add AlertDialog & restart game

--- P8_1120/app/src/main/java/ncku/geomatics/p8_1120/MainActivity.java
+++ P8_1120/app/src/main/java/ncku/geomatics/p8_1120/MainActivity.java
@@ -1,9 +1,11 @@
 package ncku.geomatics.p8_1120;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -18,20 +20,22 @@ import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity
+        implements SensorEventListener,
+        DialogInterface.OnClickListener {
 
     ImageView ivFish, ivPenguin, ivBear;
-    ConstraintLayout constraintLayout;
     ConstraintLayout.LayoutParams paramsFish, paramsPenguin, paramsBear;
+    Intent it = new Intent();
     int count = 0;
     TextView tvC;
+    Chronometer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //constraintLayout = findViewById(R.id.root);
         //設置重力感測器
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         Sensor srA = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -45,36 +49,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         paramsFish = (ConstraintLayout.LayoutParams) ivFish.getLayoutParams();
         paramsBear = (ConstraintLayout.LayoutParams) ivBear.getLayoutParams();
         //開啟首頁
-        Intent it = new Intent();
         it.setClass(this, PopupActivity.class);
         startActivityForResult(it, 99);
 
+        timer = findViewById(R.id.chronometer);
         tvC = findViewById(R.id.textViewCount);
-        tvC.setText(Integer.toString(count));
     }
-
-//    public void addImage() {
-//        ImageView iv = new ImageView(this);
-//        iv.setImageResource(R.drawable.fish);
-//        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) iv.getLayoutParams();
-//        params.topToTop=0;
-//        params.bottomToBottom=0;
-//        params.endToEnd=0;
-//        params.startToStart=0;
-//        do {
-//            //隨機設定影像XY座標
-//            Random random = new Random();
-//            params.verticalBias = ((float) random.nextInt(97) + 2) / 100;
-//            params.horizontalBias = ((float) random.nextInt(97) + 2) / 100;
-//            iv.setLayoutParams(params);
-//            //計時器重新開始計算
-//            Chronometer timer = findViewById(R.id.chronometer);
-//            timer.setBase(SystemClock.elapsedRealtime());
-//            timer.start();
-//        } while (params.horizontalBias < 0.6 && params.horizontalBias > 0.4 && params.verticalBias < 0.6 && params.verticalBias > 0.4);
-//        constraintLayout.addView(iv);
-//        setContentView(constraintLayout);
-//    }
 
     public void setImagePosition(ConstraintLayout.LayoutParams params, ImageView imageView) {
         //當影像在中心區域時，重新設置座標
@@ -99,9 +79,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 //設定星星位置
                 setImagePosition(paramsFish, ivFish);
                 //計時器重新開始計算
-                Chronometer timer = findViewById(R.id.chronometer);
                 timer.setBase(SystemClock.elapsedRealtime());
                 timer.start();
+                tvC.setText(count + "");
+                ivFish.setVisibility(View.VISIBLE);
             }
         }
     }
@@ -111,7 +92,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //移動小精靈
         float x = event.values[0];
         float y = event.values[1];
-        paramsPenguin = (ConstraintLayout.LayoutParams) ivPenguin.getLayoutParams();
         if (x > 7.5) {
             x = (float) 7.5;
         } else if (x < -7.5) {
@@ -126,19 +106,54 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         paramsPenguin.horizontalBias = (float) ((7.5 - x) / 15);
         ivPenguin.setLayoutParams(paramsPenguin);
         //小精靈吃到星星以後重新設定星星位置
-        if (Math.abs(paramsPenguin.verticalBias - paramsFish.verticalBias) < 0.05 && Math.abs(paramsPenguin.horizontalBias - paramsFish.horizontalBias) < 0.05) {
+        if (Math.abs(paramsPenguin.verticalBias - paramsFish.verticalBias) < 0.06
+                && Math.abs(paramsPenguin.horizontalBias - paramsFish.horizontalBias) < 0.06) {
             count++;
-            tvC.setText(Integer.toString(count));
+            tvC.setText(count + "");
             setImagePosition(paramsFish, ivFish);
-            if(count>=5){
+            if (count >= 5) {
                 ivBear.setVisibility(View.VISIBLE);
                 setImagePosition(paramsBear, ivBear);
             }
+        }
+        if (Math.abs(paramsPenguin.verticalBias - paramsBear.verticalBias) < 0.04
+                && Math.abs(paramsPenguin.horizontalBias - paramsBear.horizontalBias) < 0.04) {
+            String endTime = timer.getText().toString();
+            new AlertDialog.Builder(this)
+                    .setIcon(R.drawable.bomb)
+                    .setTitle("遊戲結束").setMessage("花費時間：" + endTime + "\n吃魚次數：" + count)
+                    .setPositiveButton("重新開始", this)
+                    .setNegativeButton("返回首頁", this)
+                    .setCancelable(false)
+                    .show();
+            timer.stop();
+            paramsFish.verticalBias = 0;
+            paramsFish.horizontalBias = 0;
+            ivFish.setVisibility(View.GONE);
+            paramsBear.verticalBias = 0;
+            paramsBear.horizontalBias = 0;
+            ivBear.setVisibility(View.GONE);
+            count = 0;
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        if (which == DialogInterface.BUTTON_POSITIVE) {
+            //設定星星位置
+            setImagePosition(paramsFish, ivFish);
+            //計時器重新開始計算
+            timer.setBase(SystemClock.elapsedRealtime());
+            timer.start();
+            tvC.setText(count + "");
+            ivFish.setVisibility(View.VISIBLE);
+        } else if (which == DialogInterface.BUTTON_NEGATIVE) {
+            startActivityForResult(it, 99);
+        }
     }
 }
