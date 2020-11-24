8744e230991f227d02c2f8add522c32bd28f098f
Vinnie <Lazurite259@users.noreply.github.com>
2020-11-24 15:48:34 +0800

    set sensor and replace star when elf touches the star and restart timer

--- P8_1120/app/src/main/java/ncku/geomatics/p8_1120/MainActivity.java
+++ P8_1120/app/src/main/java/ncku/geomatics/p8_1120/MainActivity.java
@@ -4,47 +4,53 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
+import android.os.SystemClock;
 import android.widget.Chronometer;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     ImageView ivStar, ivElf, ivBomb;
-    ConstraintLayout.LayoutParams params;
+    ConstraintLayout.LayoutParams paramsStar, paramsElf, paramsBomb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ivStar = findViewById(R.id.imageViewStar);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor srA = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, srA, SensorManager.SENSOR_DELAY_NORMAL);
+
         ivElf = findViewById(R.id.imageViewElf);
+        ivStar = findViewById(R.id.imageViewStar);
         ivBomb = findViewById(R.id.imageViewBomb);
+        paramsElf = (ConstraintLayout.LayoutParams) ivElf.getLayoutParams();
+        paramsStar = (ConstraintLayout.LayoutParams) ivStar.getLayoutParams();
+        paramsBomb = (ConstraintLayout.LayoutParams) ivBomb.getLayoutParams();
         Intent it = new Intent();
         it.setClass(this, PopupActivity.class);
         startActivityForResult(it, 99);
     }
 
-    public void setImagePosition(ImageView imageView) {
-        Random random = new Random();
-        params = (ConstraintLayout.LayoutParams) imageView.getLayoutParams();
-        params.verticalBias = ((float) random.nextInt(99) + 1) / 100;
-        params.horizontalBias = ((float) random.nextInt(99) + 1) / 100;
-        imageView.setLayoutParams(params);
+    public void setImagePosition(ConstraintLayout.LayoutParams params, ImageView imageView) {
+        do {
+            Random random = new Random();
+            params.verticalBias = ((float) random.nextInt(97) + 2) / 100;
+            params.horizontalBias = ((float) random.nextInt(97) + 2) / 100;
+            imageView.setLayoutParams(params);
+            Chronometer timer = findViewById(R.id.chronometer);
+            timer.setBase(SystemClock.elapsedRealtime());
+            timer.start();
+        } while (paramsStar.horizontalBias < 0.6 && paramsStar.horizontalBias > 0.4 && paramsStar.verticalBias < 0.6 && paramsStar.verticalBias > 0.4);
     }
 
     @Override
@@ -55,13 +61,38 @@ public class MainActivity extends AppCompatActivity {
             if (data != null) {
                 str = data.getStringExtra("content");
             }
-            do {
-                setImagePosition(ivStar);
-                Chronometer timer = findViewById(R.id.chronometer);
-                if (str != null && str.equals("start")) {
-                    timer.start();
-                }
-            } while (params.horizontalBias < 0.4 && params.horizontalBias > 0.6 && params.verticalBias < 0.4 && params.verticalBias > 0.6);
+            if (str != null && str.equals("start")) {
+                setImagePosition(paramsStar, ivStar);
+            }
         }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        paramsElf = (ConstraintLayout.LayoutParams) ivElf.getLayoutParams();
+        if (x > 7.5) {
+            x = (float) 7.5;
+        } else if (x < -7.5) {
+            x = (float) -7.5;
+        }
+        if (y > 7.5) {
+            y = (float) 7.5;
+        } else if (y < -7.5) {
+            y = (float) -7.5;
+        }
+        paramsElf.verticalBias = (float) ((7.5 + y) / 15);
+        paramsElf.horizontalBias = (float) ((7.5 - x) / 15);
+        ivElf.setLayoutParams(paramsElf);
+
+        if (Math.abs(paramsElf.verticalBias - paramsStar.verticalBias) < 0.05 && Math.abs(paramsElf.horizontalBias - paramsStar.horizontalBias) < 0.05) {
+            setImagePosition(paramsStar, ivStar);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
