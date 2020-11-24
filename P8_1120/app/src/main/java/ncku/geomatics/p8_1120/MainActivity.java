2441fada7926fd139ecfa42deb19684e21c92505
Vinnie <Lazurite259@users.noreply.github.com>
2020-11-24 18:13:20 +0800

    change image & count fish add bear

--- P8_1120/app/src/main/java/ncku/geomatics/p8_1120/MainActivity.java
+++ P8_1120/app/src/main/java/ncku/geomatics/p8_1120/MainActivity.java
@@ -14,64 +14,67 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    ImageView ivFish1, ivFish2, ivFish3, ivFish4, ivPenguin, ivBomb1;
+    ImageView ivFish, ivPenguin, ivBear;
     ConstraintLayout constraintLayout;
-    ConstraintLayout.LayoutParams paramsFish1, paramsPenguin, paramsBear;
+    ConstraintLayout.LayoutParams paramsFish, paramsPenguin, paramsBear;
+    int count = 0;
+    TextView tvC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        constraintLayout = findViewById(R.id.root);
+        //constraintLayout = findViewById(R.id.root);
         //設置重力感測器
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         Sensor srA = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, srA, SensorManager.SENSOR_DELAY_NORMAL);
         //取得影像控制權
         ivPenguin = findViewById(R.id.imageViewPenguin);
-        ivFish1 = findViewById(R.id.imageViewFish1);
-//        ivFish2 = findViewById(R.id.imageViewFish2);
-//        ivFish3 = findViewById(R.id.imageViewFish3);
-//        ivFish4 = findViewById(R.id.imageViewFish4);
-//        ivBomb1 = findViewById(R.id.imageViewBear1);
+        ivFish = findViewById(R.id.imageViewFish);
+        ivBear = findViewById(R.id.imageViewBear);
         //取得影像參數
         paramsPenguin = (ConstraintLayout.LayoutParams) ivPenguin.getLayoutParams();
-        paramsFish1 = (ConstraintLayout.LayoutParams) ivFish1.getLayoutParams();
-        paramsBear = (ConstraintLayout.LayoutParams) ivBomb1.getLayoutParams();
+        paramsFish = (ConstraintLayout.LayoutParams) ivFish.getLayoutParams();
+        paramsBear = (ConstraintLayout.LayoutParams) ivBear.getLayoutParams();
         //開啟首頁
         Intent it = new Intent();
         it.setClass(this, PopupActivity.class);
         startActivityForResult(it, 99);
+
+        tvC = findViewById(R.id.textViewCount);
+        tvC.setText(Integer.toString(count));
     }
 
-    public void addImage() {
-        ImageView iv = new ImageView(this);
-        iv.setImageResource(R.drawable.fish);
-        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) iv.getLayoutParams();
-        params.topToTop=0;
-        params.bottomToBottom=0;
-        params.endToEnd=0;
-        params.startToStart=0;
-        do {
-            //隨機設定影像XY座標
-            Random random = new Random();
-            params.verticalBias = ((float) random.nextInt(97) + 2) / 100;
-            params.horizontalBias = ((float) random.nextInt(97) + 2) / 100;
-            iv.setLayoutParams(params);
-            //計時器重新開始計算
-            Chronometer timer = findViewById(R.id.chronometer);
-            timer.setBase(SystemClock.elapsedRealtime());
-            timer.start();
-        } while (params.horizontalBias < 0.6 && params.horizontalBias > 0.4 && params.verticalBias < 0.6 && params.verticalBias > 0.4);
-        constraintLayout.addView(iv);
-        setContentView(constraintLayout);
-    }
+//    public void addImage() {
+//        ImageView iv = new ImageView(this);
+//        iv.setImageResource(R.drawable.fish);
+//        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) iv.getLayoutParams();
+//        params.topToTop=0;
+//        params.bottomToBottom=0;
+//        params.endToEnd=0;
+//        params.startToStart=0;
+//        do {
+//            //隨機設定影像XY座標
+//            Random random = new Random();
+//            params.verticalBias = ((float) random.nextInt(97) + 2) / 100;
+//            params.horizontalBias = ((float) random.nextInt(97) + 2) / 100;
+//            iv.setLayoutParams(params);
+//            //計時器重新開始計算
+//            Chronometer timer = findViewById(R.id.chronometer);
+//            timer.setBase(SystemClock.elapsedRealtime());
+//            timer.start();
+//        } while (params.horizontalBias < 0.6 && params.horizontalBias > 0.4 && params.verticalBias < 0.6 && params.verticalBias > 0.4);
+//        constraintLayout.addView(iv);
+//        setContentView(constraintLayout);
+//    }
 
     public void setImagePosition(ConstraintLayout.LayoutParams params, ImageView imageView) {
         //當影像在中心區域時，重新設置座標
@@ -94,8 +97,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
             if (str != null && str.equals("start")) {
                 //設定星星位置
-                //setImagePosition(paramsFish1, ivFish1);
-                addImage();
+                setImagePosition(paramsFish, ivFish);
+                //計時器重新開始計算
+                Chronometer timer = findViewById(R.id.chronometer);
+                timer.setBase(SystemClock.elapsedRealtime());
+                timer.start();
             }
         }
     }
@@ -120,9 +126,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         paramsPenguin.horizontalBias = (float) ((7.5 - x) / 15);
         ivPenguin.setLayoutParams(paramsPenguin);
         //小精靈吃到星星以後重新設定星星位置
-//        if (Math.abs(paramsPenguin.verticalBias - paramsFish1.verticalBias) < 0.05 && Math.abs(paramsPenguin.horizontalBias - paramsFish1.horizontalBias) < 0.05) {
-//            setImagePosition(paramsFish1, ivFish1);
-//        }
+        if (Math.abs(paramsPenguin.verticalBias - paramsFish.verticalBias) < 0.05 && Math.abs(paramsPenguin.horizontalBias - paramsFish.horizontalBias) < 0.05) {
+            count++;
+            tvC.setText(Integer.toString(count));
+            setImagePosition(paramsFish, ivFish);
+            if(count>=5){
+                ivBear.setVisibility(View.VISIBLE);
+                setImagePosition(paramsBear, ivBear);
+            }
+        }
     }
 
     @Override
