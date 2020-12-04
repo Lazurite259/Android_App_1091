5f39d4275881d683b9ac6bf025adcd6fc3e5b1ce
Vinnie <Lazurite259@users.noreply.github.com>
2020-12-04 14:55:49 +0800

    zoom to current position and add marker

--- P1127/app/src/main/java/ncku/geomatics/p1127/MainActivity.java
+++ P1127/app/src/main/java/ncku/geomatics/p1127/MainActivity.java
@@ -7,17 +7,30 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+import java.util.Locale;
+
 //Step 1
 public class MainActivity extends AppCompatActivity
-        implements LocationListener {
+        implements LocationListener, OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,24 +52,66 @@ public class MainActivity extends AppCompatActivity
             // for ActivityCompat#requestPermissions for more details.
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION},
+                            Manifest.permission.ACCESS_FINE_LOCATION},
                     200);
             return;
         }
-        String provider =locationManager.getBestProvider(new Criteria(),true);
-        ((TextView)findViewById(R.id.textView)).setText(provider);
-        locationManager.requestLocationUpdates("gps", 5000, 5, this);
+        String provider = locationManager.getBestProvider(new Criteria(), true);
+        ((TextView) findViewById(R.id.textView)).setText(provider);
+        locationManager.requestLocationUpdates(provider, 5000, 5, this);
+
+        SupportMapFragment smf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        smf.getMapAsync(this);
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
         //Step 3
-        Double lat = location.getLatitude();
-        Double lng = location.getLongitude();
-        Double alt = location.getAltitude();
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        double alt = location.getAltitude();
 
         ((EditText) findViewById(R.id.editTextNumberLat)).setText(lat + "");
         ((EditText) findViewById(R.id.editTextNumberLong)).setText(lng + "");
         ((EditText) findViewById(R.id.editTextNumberAlt)).setText(alt + "");
+
+        if(map!=null){
+            LatLng currentLocation=new LatLng(lat,lng);
+            map.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+            map.clear();
+            MarkerOptions markerOptions=new MarkerOptions();
+            markerOptions.position(currentLocation).title("NCKU");
+            map.addMarker(markerOptions);
+        }
+    }
+
+    public void onClick(View v) {
+        Geocoder geo = new Geocoder(this, Locale.getDefault());
+        try {
+            double lat = Double.parseDouble(((EditText) findViewById(R.id.editTextNumberLat)).getText().toString());
+            double lng = Double.parseDouble(((EditText) findViewById(R.id.editTextNumberLong)).getText().toString());
+            List<Address> address = geo.getFromLocation(lat, lng, 1);
+//            address = geo.getFromLocationName("台北車站", 1);
+//            String str = address.get(0);
+            Address address1 = address.get(0);
+//            String str = address1.getLatitude() + ", " + address1.getLongitude();
+            String str = "";
+            for (int i = 0; i <= address1.getMaxAddressLineIndex(); i++) {
+                str += address1.getAddressLine(i);
+            }
+            ((TextView) findViewById(R.id.textView)).setText(str);
+        } catch (Exception e) {
+            ((TextView) findViewById(R.id.textView)).setText(e.toString());
+        }
+    }
+
+    GoogleMap map = null;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(23, 120.22)));
+        map.moveCamera(CameraUpdateFactory.zoomTo(17));
     }
 }
