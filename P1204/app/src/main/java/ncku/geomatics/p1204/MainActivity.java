99f677cb93822f1d88fb9666de3b320f7ae2014b
Vinnie <Lazurite259@users.noreply.github.com>
2020-12-18 14:36:35 +0800

    fix image rotation and select image from local storage

--- P1204/app/src/main/java/ncku/geomatics/p1204/MainActivity.java
+++ P1204/app/src/main/java/ncku/geomatics/p1204/MainActivity.java
@@ -5,19 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
-
-import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
@@ -33,13 +35,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    Uri imgUri;
+    Uri imgUrl;
 
     public void onClick(View v) {
-        imgUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new ContentValues());
+        imgUrl = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new ContentValues());
         Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        it.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+        it.putExtra(MediaStore.EXTRA_OUTPUT, imgUrl);
         startActivityForResult(it, 100);
+    }
+
+    public void onClick2(View v) {
+        Intent it = new Intent(Intent.ACTION_GET_CONTENT);
+        it.setType("image/*");
+        startActivityForResult(it, 199);
     }
 
     @Override
@@ -51,10 +59,25 @@ public class MainActivity extends AppCompatActivity {
             //存到外部儲存空間，Intent不會有照片回傳，要用Content Resolver解碼
             Bitmap bmp = null;
             try {
-                bmp= BitmapFactory.decodeStream(getContentResolver().openInputStream(imgUri),null,null);
-            }
-            catch (Exception e){
+                BitmapFactory.Options opt = new BitmapFactory.Options();
+                opt.inJustDecodeBounds = true;
+                bmp = BitmapFactory.decodeStream(getContentResolver().openInputStream(imgUrl), null, opt);
+                opt.inSampleSize = 10;
+                opt.inJustDecodeBounds = false;
+                bmp = BitmapFactory.decodeStream(getContentResolver().openInputStream(imgUrl), null, opt);
 
+                Matrix matrix = new Matrix();
+                matrix.postRotate(90);
+                Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, true);
+            } catch (Exception e) {
+            }
+            ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bmp);
+        } else if (resultCode == RESULT_OK && requestCode == 199) {
+            imgUrl = data.getData();
+            Bitmap bmp = null;
+            try {
+                bmp = BitmapFactory.decodeStream(getContentResolver().openInputStream(imgUrl), null, null);
+            } catch (Exception e) {
             }
             ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bmp);
         }
